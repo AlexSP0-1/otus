7e65ae19bf45d362009dcd24184b17f48b93f67a
Alexey Saprunov <workernight@yandex.ru>
2023-01-15 14:42:03 +0400

    chore: add test for sorting function

--- ip-filter/ip_filter_tests.cpp
+++ ip-filter/ip_filter_tests.cpp
@@ -1,6 +1,24 @@
+#include "ip_filter_lib.h"
 #include "gtest/gtest.h"
 
-TEST(LexacalSortTest, testSort) {}
+TEST(LexacalSortTest, testSort)
+{
+    std::vector<std::vector<std::string>> testingVector = {{"4", "1", "1", "4"},
+                                                           {"5", "2", "1", "2"},
+                                                           {"5", "1", "1", "2"},
+                                                           {"5", "2", "2", "2"},
+                                                           {"1", "1", "1", "3"}};
+
+    std::vector<std::vector<std::string>> resultVector = {{"5", "2", "2", "2"},
+                                                          {"5", "2", "1", "2"},
+                                                          {"5", "1", "1", "2"},
+                                                          {"4", "1", "1", "4"},
+                                                          {"1", "1", "1", "3"}};
+
+    lexicalSort(testingVector);
+
+    EXPECT_EQ(testingVector, resultVector);
+}
 
 int main(int argc, char **argv)
 {
