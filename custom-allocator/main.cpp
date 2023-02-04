386c1142626270e408ada533550f0ef862ac8093
Alexey Saprunov <workernight@yandex.ru>
2023-02-04 21:47:36 +0400

    chore: implements main

--- custom-allocator/main.cpp
+++ custom-allocator/main.cpp
@@ -2,7 +2,9 @@
 #include "customlinkedlist.h"
 #include "simplestrategy.h"
 
-static const int SZ = 15; //chunk size
+#include <map>
+
+static const int SZ = 10;
 
 int MyFactorial(int n)
 {
@@ -16,4 +18,61 @@ int MyFactorial(int n)
     return current;
 }
 
-int main(int, char **) {}
+void printMap(std::map<int, int> &m)
+{
+    for (const auto &[key, value] : m)
+    {
+        std::cout << key << " " << value << std::endl;
+    }
+}
+
+template<typename T>
+using allocatorSimple = Custom_allocator<T, SZ, simple_strategy>;
+
+int main(int, char **)
+{
+    //std::map with std::allocator
+    std::map<int, int> mFactorial;
+    for (int i = 0; i < SZ; i++)
+    {
+        mFactorial.try_emplace(i, MyFactorial(i));
+    }
+
+    printMap(mFactorial);
+
+    //using custom allocator
+    std::map<int, int, std::less<int>, Custom_allocator<int, SZ, simple_strategy>> mCustomFactorial;
+
+    for (int i = 0; i < SZ; i++)
+    {
+        mCustomFactorial.try_emplace(i, MyFactorial(i));
+    }
+
+    printMap(mFactorial);
+
+    //using custom linked list with standart allocator
+    CustomLinkedList<int, std::allocator<int>> customList;
+
+    for (int i = 0; i < SZ; i++)
+    {
+        customList.add(i);
+    }
+
+    for (auto it = customList.begin(); it != customList.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    //using custom linked list with custom allocator
+    CustomLinkedList<int, allocatorSimple<int>> customListWithCustomAlloc;
+
+    for (int i = 0; i < SZ; i++)
+    {
+        customListWithCustomAlloc.add(i);
+    }
+
+    for (auto it = customListWithCustomAlloc.begin(); it != customListWithCustomAlloc.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+}
