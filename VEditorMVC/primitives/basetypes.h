0d9de88fd5d17e26a47c9a513de691940d3718e4
Alexey Saprunov <workernight@yandex.ru>
2023-03-05 22:47:27 +0400

    chore: add graphical primitives

---
+++ VEditorMVC/primitives/basetypes.h
@@ -0,0 +1,17 @@
+#ifndef BASETYPES_H
+#define BASETYPES_H
+
+struct Point
+{
+    int xCoord = 0;
+    int yCoord = 0;
+};
+
+enum PrimitiveTypes
+{
+    POINT,
+    LINE,
+    CIRCLE
+};
+
+#endif
---
+++ VEditorMVC/primitives/primitives.h
@@ -0,0 +1,57 @@
+#ifndef PRIMITIVES_H
+#define PRIMITIVES_H
+
+#include "igraphprimitive.h"
+
+class GraphPrimitiveObject : public IGraphPrimitiveObject
+{
+public:
+    GraphPrimitiveObject(const Point &location);
+
+    virtual ~GraphPrimitiveObject();
+
+public:
+    void draw(IPainter *painter) const override;
+    void setEnd(const Point &endPoint) override;
+    void setRadius(const int radius) override;
+    void setLocation(const Point &location);
+
+protected:
+    Point m_location;
+};
+
+class PointPrimitive : public GraphPrimitiveObject
+{
+public:
+    PointPrimitive(const Point &location);
+
+    void draw(IPainter *painter) const override;
+};
+
+class LinePrimitive : public GraphPrimitiveObject
+{
+public:
+    LinePrimitive(const Point &beginPoint, const Point &endPoint);
+
+    void draw(IPainter *painter) const override;
+
+    void setEnd(const Point &endPoint);
+
+private:
+    Point m_endPoint;
+};
+
+class CirclePrimitive : public GraphPrimitiveObject
+{
+public:
+    CirclePrimitive(const Point &location, const int radius);
+
+    void setRadius(const int radius) override;
+
+    void draw(IPainter *painter) const override;
+
+private:
+    int m_radius = 0;
+};
+
+#endif
