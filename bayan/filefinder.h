96c7f9e18221f81c44c23b14650be34460afc698
Alexey Saprunov <workernight@yandex.ru>
2023-05-28 20:42:03 +0400

    chore: add filefinder and ihandler classes

---
+++ bayan/filefinder.h
@@ -0,0 +1,30 @@
+#ifndef FILEFINDER_H
+#define FILEFINDER_H
+
+#include <filesystem>
+#include <memory>
+
+#include "ihandler.h"
+#include <options/ioptions.h>
+
+class FileFinder
+{
+public:
+    FileFinder(std::shared_ptr<IOptions> options);
+
+    void scan();
+
+    void out(std::shared_ptr<IHandler> handler);
+
+private:
+    bool isMatchMasks(const std::string &filename) const;
+
+    bool isPathExcluded(const std::filesystem::recursive_directory_iterator &it);
+
+private:
+    std::shared_ptr<IOptions> m_options;
+
+    std::vector<std::pair<std::filesystem::path, size_t>> m_fileList;
+};
+
+#endif // FILEFINDER_H
